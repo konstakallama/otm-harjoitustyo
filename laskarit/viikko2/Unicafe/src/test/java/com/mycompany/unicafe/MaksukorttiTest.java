deda702325d882a07f2cb1b4ca1da0950657692a
Konsta Kallama <kallama.konsta@gmail.com>
2018-03-22 01:56:05 +0200

    testit lisätty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,43 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        this.tarkistaSaldo(10);
+    }
+    
+    @Test
+    public void lataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(5);
+        this.tarkistaSaldo(15);
+    }
+    
+    private void tarkistaSaldo(int maara) {
+        assertTrue(kortti.saldo() == maara);
+    }
+    
+    @Test
+    public void saldoVahanee() {
+        kortti.otaRahaa(5);
+        this.tarkistaSaldo(5);
+    }
+    
+    @Test
+    public void saldoEiMuutuJosEiRahaa() {
+        kortti.otaRahaa(11);
+        this.tarkistaSaldo(10);
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrue() {
+        assertTrue(kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalse() {
+        assertFalse(kortti.otaRahaa(15));
+    }
+    
+    
 }
